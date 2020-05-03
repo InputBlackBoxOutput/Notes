2f2a95eb7f4428b8e2d1930206e81a581db9126e
InputBlackBoxOutput <53337979+InputBlackBoxOutput@users.noreply.github.com>
2020-05-03 21:38:13 +0530

    Python3 and C notes updated

--- c.c
+++ c.c
@@ -86,5 +86,107 @@ scanf("%d", &variable1);                            //Single variable
 scanf("%d%lf", &variable1, &variable2);             //Multiple variables
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
+// Variables and primitive datatypes
 
+int i = 10;
+float f = 1.2;
+double d = 20.20;
+char c = 'A';
+
+//Constants (These are variables that never change value i.e. become constant)
+const int no_of_days = 7;
+const float pi = 3.142;
+
+//Can variable store negative numbers? Yes => 'signed'     (Default)
+//                                     No =>  'unsigned'
+
+signed int temperature = -5;
+unsigned int number_of_people = 10; 
+
+//Local and global variable
+//  Local  => inside a scope  
+//  Global => outside a scope
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Arrays
+// Creating an array
+int arr1[10];                       // Integer array of size 10
+char c[]={'A','B','C','D','E'};     // Character array containg first five alphabets
+
+// Accessing and modifying elements in an array
+arr1[0]=10;                  // Modifying the first element to 10
+arr1[3]=5;                   // Modifying the fourth element to 5
+
+printf("First element of array = %d", arr1[0]);     // Accessing the first element
+
+// Creating a multi-dimensional array
+int arr2[3][2]={ {1,2},
+                 {3,4},
+                 {5,6} };                       // Integer array of size 3x2
+
+// Accessing and modifying elements in an multi-dimension array
+arr2[0][0] = 10;                  
+arr2[3][1] = 5;                   
+
+cout << arr2[0][0] << endl; 
+    
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Strings
+//-----------------------------------------------------------------------------------------------
+// Useful character related functions
+#include<ctype.h>
+isaplha(ch);  isapnum(ch); isdigit(ch);
+isupper(ch);  islower(ch);
+
+tolower(ch); toupper(ch);
+
+//-----------------------------------------------------------------------------------------------
+// C style strings (\0 (null) terminated strings)
+char name[]={"Rutuparn Pawar"};
+
+char fstName[8];
+fstName = "Rutuparn";            // This will not work, instead use strcpy(fstName, "Rutuparn");
+
+scanf("%s", &fstName);          // Getting string from user
+
+// Note the above functions can be found in #include<strings.h>
+strcpy(fstName, "Rutuparn");   
+strcat(fstName," Pawar");
+cout << strlen(fstName);
+strcmp('Hello', 'hello');
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Functions (Provides abstraction)
+// Function with only parameters
+void greetUser(string name) {
+	printf("Hello, User");
+}
+
+// Function with only return
+int generateRandomNumber() {
+	return rand()%7;             // Return random number between 0 and 6 (Similar to a dice)
+}
+
+// Function with return and parameters
+int greaterNum(int num1, int num2) {
+	if(num1 > num2)
+		return num1;
+	else
+		return num2;
+}
+
+// Calling a function
+void areaCircle(void);  // Function prototype
+
+int main() {
+	areaCircle();       // Calling the function
+	return 0;
+}
+
+void areaCircle() {
+	double r;
+	printf("Enter radius of circle:");
+	scanf("%lf", &r);
+	printf("Area of circle:%lf", (3.142*r*r));
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////
--- python3.py
+++ python3.py
@@ -33,11 +33,29 @@ def func():
 func()
 
 #---------------------------------------------------------------------------------------------------
+# Modules
+
+# Importing modules
+import math
+
+# Importing modules with alias
+import pickle as p
+
+# Importing specific sections from a module
+from math import sqrt
+
+#---------------------------------------------------------------------------------------------------
 # Basic input/output
 print("String Text")                 # Newline
 print("String", end="")              # No newline
 
-variable1 = input("Enter a number:")                                                #Single variable
+# Printing in a formatted manner
+name = 'Rutuparn Pawar'
+age = 20
+print("My name is: {}, and my age is: {}".format(name, age))   # Using .format
+print(f"My name is: {name}, and my age is: {age}")             # Using fstring
+
+variable1 = int(input("Enter a number:"))                                         #Single integer variable
 variable1, variable2 = input("Enter 2 number (with space in between):").split()   #Multiple variable
 
 #---------------------------------------------------------------------------------------------------
@@ -48,5 +66,169 @@ name = 'InputBlackBoxOutput'   # String
 isClosed = True                # Boolean  
 
 #---------------------------------------------------------------------------------------------------
+# Arithmetic operations
+a = 10
+b = 5
+print("Addition = ", a+b)         # Addition
+print("Substraction= ",(a-b))     # Substraction
+print("Division= ",(a/b))         # Division
+print("Multiplication= ",(a*b))   # Multiplication
+print("Remainder= ",(a%b))        # Remainder operator
+print("Power="(a**b))             # Power
 
 #---------------------------------------------------------------------------------------------------
+# Relational operators
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+
+#---------------------------------------------------------------------------------------------------
+# Logical operators
+print(a and b)
+print(a or b)
+
+#---------------------------------------------------------------------------------------------------
+# Strings and escape characters
+text = "Python is fun!"
+speech1 = "Mark said, \"Python is fun \" "
+speech2 = 'Mark said, "Python is fun"'
+
+escChar = 'a \n b \t c \\ d \" e \' f'   # Only a few shown 
+
+# Useful string functions
+print(text.lower())
+print(text.upper())
+print(text.split())  # By default split works with space
+
+# String concatenation
+str1 = 'My name is '
+str2 = 'Rutuparn Pawar'
+print(str1+str2)
+
+# Getting char from string using bracket notation
+print(text[0])     # 'P' printed on screen
+
+#---------------------------------------------------------------------------------------------------
+# Lists
+lis1 = [1,2,3,4]                       # Same datatypes
+lis2 = [1,True,'InputBlackBoxOutput']  # Different datatypes
+lis3 = list(range(0,100))              # List with values 0 to 99
+
+# Adding to the end of a list
+print(lis1.append(5))    
+
+# Removing from the end of the list
+print(lis1.pop())
+
+#Accessing an element from the list
+print(lis2[2])          # 3rd element = 'InputBlackBoxOutput'
+
+# List comprehension
+lis = [x**3 for x in range(0,4)]    # lis = [0, 1, 8, 27]
+
+#---------------------------------------------------------------------------------------------------
+# Nested list
+nestedList = [[1,2,3], [4,5,6], [7,8,9]]
+
+#Accessing an element from the list
+print(nestedList[0][2])          # 1st list 3rd element = 'InputBlackBoxOutput'
+
+#---------------------------------------------------------------------------------------------------
+# Tuples (Look at a tuple as a fixed list)
+tup = (1,2,3)
+
+# Changing a tuple is not possible!
+# t[0] = 20 
+
+# Accessing value from a tuple
+print(t[0])
+#---------------------------------------------------------------------------------------------------
+# Dictonary (Used to store key-value pairs)
+dic = {'a':1, 'b':True, 'c':'String'}
+
+# Changing dictionary value associated with a key
+dic['b'] = False
+
+# Accessing dictionary value for a key
+print(dic['c'])
+
+# Getting dictionary keys
+print('Keys = ', dic.keys())
+
+#---------------------------------------------------------------------------------------------------
+# Conditional statements
+# if
+if a>b:
+	print(f'{a} is greater than {b}')
+
+# if else
+if a>b:
+	print(f'{a} is greater than {b}')
+else:
+	print(f'{b} is greater than {a}')
+
+#if elif else
+if a>b:
+	print(f'{a} is greater than {b}')
+elif a==b:
+	print(f'{a} is equal to {b}')
+else:
+	print(f'{b} is greater than {a}')
+
+#---------------------------------------------------------------------------------------------------
+# Iteration statements
+# for with range
+for x in range(0, 10):
+	print(x)
+
+# for with list
+for each in ['one', 'two', 'three']:
+	print(x)
+
+# while
+i=0
+while(i<10):
+	print(i)
+	i+=1
+
+#---------------------------------------------------------------------------------------------------
+# Functions
+
+# import math
+def vectorMagnitude(x, y, z):
+	return math.sqrt(x**2 + y**2 + z**2)
+
+print(vectorMagnitude(10, 10, 10))     # O/P = 17.32
+
+# Function with default parameter
+def greetUser(name='User'):
+	print('Hello ' + name)
+
+greetUser()             # O/P => Hello User
+greetUser('Rutuparn')   # O/P => Hello Rutuparn
+#---------------------------------------------------------------------------------------------------
+# Lamda expressions (AKA anonymous function)
+# Syntax => something = lambda input:output
+square = lambda x: x**2
+
+#---------------------------------------------------------------------------------------------------
+# Very special functions 
+# (Please see lambda functions before viewing this section)
+test = [1, 2, 3, 4]
+
+# Apply to all elements
+test = list(map(lambda x:x*100, test))      # Returns list with all elements multiplied by 100
+# Apply condition on a list
+test = list(filter(lambda x:x>=3, test))    # Returns list element >= 3 
+# Find a computation using all list elements
+# from functools import reduce
+test = reduce(lambda x,y:x+y, test)   # Returns sum of list elements
+# Find unique values 
+set(test)
+ 
+#---------------------------------------------------------------------------------------------------
+
+
